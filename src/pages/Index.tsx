0f51e893a0399bd96cb50c22335683631fc89d3c
eomseona <eomsa98@tukorea.ac.kr>
2024-07-17 19:26:12 +0900

    docs : 임시 데이터 생성

---
+++ src/pages/Index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Index = () => {
+  return <div>임시 파일입니다</div>;
+};
+
+export default Index;
---
+++ src/utils/Index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>임시 파일입니다</div>;
+};
+
+export default index;
