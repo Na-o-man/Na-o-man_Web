e3f9a9fec8c05e0298c1624648b6be87b79874b4
eomseona <eomsa98@tukorea.ac.kr>
2024-07-19 00:46:07 +0900

    Chore: Icon 디렉토리 정리 및 컴포넌트화

---
+++ src/assets/icon.ts
@@ -0,0 +1,29 @@
+/** svg 아이콘 리액트 컴포넌트화 */
+
+// add group
+export { ReactComponent as GroupBtn } from './icon/add_group/group_btn.svg';
+export { ReactComponent as Input } from './icon/add_group/input.svg';
+export { ReactComponent as InputGroupType } from './icon/add_group/input_group_type.svg';
+export { ReactComponent as InputName } from './icon/add_group/input_name.svg';
+export { ReactComponent as Profile } from './icon/add_group/profile.svg';
+export { ReactComponent as UnderBar } from './icon/add_group/underbar.svg';
+// common
+export { ReactComponent as Cloud } from './icon/common/cloud.svg';
+export { ReactComponent as Fly } from './icon/common/fly.svg';
+export { ReactComponent as UpperCloud } from './icon/common/uppercloud.svg';
+export { ReactComponent as UpperCloudHeavy } from './icon/common/uppercloud_heavy.svg';
+export { ReactComponent as Check } from './icon/common/button/check.svg';
+export { ReactComponent as Close } from './icon/common/button/close.svg';
+export { ReactComponent as Hamburger } from './icon/common/button/hamburger.svg';
+export { ReactComponent as BackArrow } from './icon/common/button/back_arrow.svg';
+export { ReactComponent as NextArrow } from './icon/common/button/next_arrow.svg';
+export { ReactComponent as AddBtn } from './icon/common/navigation_bar/add_btn.svg';
+export { ReactComponent as Background } from './icon/common/navigation_bar/background.svg';
+export { ReactComponent as HomeBtn } from './icon/common/navigation_bar/home_btn.svg';
+export { ReactComponent as NotificationBtn } from './icon/common/navigation_bar/notificaton_btn.svg';
+// share group
+export { ReactComponent as AddGroupBtn } from './icon/share_group/add_group_btn.svg';
+export { ReactComponent as CloudBtn } from './icon/share_group/cloud_btn.svg';
+export { ReactComponent as Folder } from './icon/share_group/folder.svg';
+export { ReactComponent as MiddleRectangle } from './icon/share_group/middle_rectangle.svg';
+export { ReactComponent as SemiCircle } from './icon/share_group/semi_circle.svg';
