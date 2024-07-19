525fb89852c43735e990b7c874aef616a79f071f
eomseona <eomsa98@tukorea.ac.kr>
2024-07-19 16:36:10 +0900

    Style: 기본 스타일 초기화

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from 'styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <GlobalStyles />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>,
 );
 
 // If you want to start measuring performance in your app, pass a function
