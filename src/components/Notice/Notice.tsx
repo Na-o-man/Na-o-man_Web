c0c0073838ee752a64be8a4254778e3635700eb6
khg5357 <rlagprud5356@gmail.com>
2024-07-23 18:31:09 +0900

    Notice 퍼블리싱 구현

---
+++ src/components/Notice/Notice.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as S from './Styles';
+import * as I from 'assets/icon';
+import Header from 'components/Header/Header';
+import { DownCloud, IndexTag, NoticeTag } from './Styles';
+import cloudRight from '../../assets/background/cloudRight.png';
+import GroupNotice from './GroupNotice/GroupNotice';
+
+const Notice = () => {
+  return (
+    <S.Layout>
+      <img
+        src={cloudRight}
+        style={{ width: '100%', position: 'absolute', zIndex: '0' }}
+      ></img>
+      <S.H>
+        <Header backarrow></Header>
+      </S.H>
+      <IndexTag></IndexTag>
+      <NoticeTag>알림</NoticeTag>
+      <S.NoticeContainer>
+        <S.NoticeBox>
+          <GroupNotice />
+          <GroupNotice />
+          <GroupNotice />
+          <GroupNotice />
+          <GroupNotice />
+          <GroupNotice />
+          <GroupNotice />
+        </S.NoticeBox>
+      </S.NoticeContainer>
+
+      <DownCloud></DownCloud>
+    </S.Layout>
+  );
+};
+export default Notice;
---
+++ src/components/Notice/Styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import * as I from 'assets/icon';
+
+export const Layout = styled.div`
+  background-color: #bbcfe5;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+export const H = styled.div`
+  z-index: 10;
+`;
+
+export const NoticeTag = styled.div`
+  position: absolute;
+  top: 14%;
+  left: 20%;
+  color: black;
+  font-weight: bolder;
+  font-size: 15px;
+  z-index: 15;
+`;
+
+export const IndexTag = styled(I.IndexTag)`
+  position: absolute;
+  top: 13%;
+  z-index: 10;
+  width: 35%;
+  height: auto;
+`;
+
+export const NoticeContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  z-index: 10;
+  padding-top: 200px;
+`;
+
+export const NoticeBox = styled.div`
+  position: relative;
+  width: 350px;
+  height: 600px;
+  gap: 10px;
+  background-color: transparent;
+  overflow: auto;
+  z-index: 10;
+  &::-webkit-scrollbar {
+    width: 7px;
+    border-radius: 7px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: white;
+    border-radius: 7px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+    border-radius: 7px;
+  }
+`;
+
+export const DownCloud = styled(I.Cloud)`
+  display: grid;
+  display: block;
+  position: absolute;
+  top: 83%;
+  left: 70%;
+  z-index: 5;
+`;
