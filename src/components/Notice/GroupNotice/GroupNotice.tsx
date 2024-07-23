d4c4012ce9ae722205342d1b2ae8aa1c5ecce00f
khg5357 <rlagprud5356@gmail.com>
2024-07-23 19:19:14 +0900

    feat: Notice 컴포넌트 구현

--- src/components/Notice/GroupNotice/GroupNotice.tsx
+++ src/components/Notice/GroupNotice/GroupNotice.tsx
@@ -5,11 +5,13 @@ import { GroupList, Profile, Text } from './Styles';
 
 const GroupNotice = () => {
   return (
+    <></>
     <S.Layout>
       <GroupList></GroupList>
       <Profile></Profile>
       <Text>[그룹명]에 [유저명] 님이 n장의 사진을 업로드했습니다.</Text>
     </S.Layout>
+    </>
   );
 };
 
