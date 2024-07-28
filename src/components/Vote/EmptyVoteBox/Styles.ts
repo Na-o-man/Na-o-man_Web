2b9415bf49e9172a5078c407e6bddc7068e687a3
khg5357 <rlagprud5356@gmail.com>
2024-07-28 13:57:51 +0900

    feat : enter페이지 라우팅 및 오류수정

---
+++ src/components/Vote/EmptyVoteBox/Styles.ts
@@ -0,0 +1,67 @@
+import * as I from 'assets/icon';
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+export const MiddleRectangle = styled(I.MiddleRectangle)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const MiddleContainer = styled.div`
+  height: 264px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  position: relative;
+  padding: 2rem 0;
+`;
+
+export const AddGroupBtn = styled(I.AddGroupBtn)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const AddGroupBtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+  height: 57px;
+`;
+
+export const Text = styled.p`
+  font-size: 1.3rem;
+  color: #fefefe;
+  text-align: center;
+`;
+
+export const AddGroupBtnText = styled.p`
+  font-size: 15px;
+  font-weight: 800;
+  color: #8ba5c1;
+  text-align: center;
+  z-index: 10;
+`;
+
+export const Cloud = styled(I.Cloud)`
+  position: absolute;
+  bottom: calc(50% - 132px - 36.5px);
+  right: calc(50% - 147.5px - 59px);
+  z-index: 10;
+  filter: drop-shadow(0px 6px 10px rgba(138, 138, 138, 0.25));
+`;
