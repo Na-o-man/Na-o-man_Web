76d992182b682f519191fcabcebdd9dd6ea39c83
eomseona <eomsa98@tukorea.ac.kr>
2024-07-31 12:23:20 +0900

    feat: 소셜 로그인 요청 주소 입력

--- src/pages/EnterMain/EnterLogin/EnterLogin.tsx
+++ src/pages/EnterMain/EnterLogin/EnterLogin.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import * as S from './Styles';
 import { Outlet } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import { loginState } from 'recoil/states/enter';
-import { useNavigate } from 'react-router-dom';
+// import { useSetRecoilState } from 'recoil';
+// import { loginState } from 'recoil/states/enter';
+// import { useNavigate } from 'react-router-dom';
 
 const EnterLogin = () => {
-  const setLoginState = useSetRecoilState(loginState);
-  const navigate = useNavigate();
+  const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+  const KAKAO_AUTH_URL = `${SERVER_URL}/oauth2/authorizatoin/kakao`;
+  const GOOGLE_AUTH_URL = `${SERVER_URL}/oauth2/authorizatoin/google`;
+  // const setLoginState = useSetRecoilState(loginState);
+  // const navigate = useNavigate();
+  // const handleGoogleLogin = () => {
+  //   console.log('Google Login Clicked');
+  //   setLoginState({ isLoggedIn: true, provider: 'google' });
+  //   navigate('clause');
+  // };
 
-  const handleGoogleLogin = () => {
-    console.log('Google Login Clicked');
-    setLoginState({ isLoggedIn: true, provider: 'google' });
-    navigate('clause');
-  };
-
-  const handleKakaoLogin = () => {
-    console.log('Kakao Login Clicked');
-    setLoginState({ isLoggedIn: true, provider: 'kakao' });
-    navigate('clause');
-  };
+  // const handleKakaoLogin = () => {
+  //   console.log('Kakao Login Clicked');
+  //   setLoginState({ isLoggedIn: true, provider: 'kakao' });
+  //   navigate('clause');
+  // };
 
   return (
     <>
       <S.LayOut>
-        <S.GoogleLogin onClick={handleGoogleLogin} />
-        <S.KakaoLogin onClick={handleKakaoLogin} />
+        <a href={GOOGLE_AUTH_URL}>
+          <S.GoogleLogin />
+        </a>
+        <a href={KAKAO_AUTH_URL}>
+          <S.KakaoLogin />
+        </a>
       </S.LayOut>
       <Outlet />
     </>
