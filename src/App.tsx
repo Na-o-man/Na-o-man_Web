370fc59c8ae85a8f1f1fcdea095aeb9cfae21d6a
eomseona <eomsa98@tukorea.ac.kr>
2024-07-31 22:58:24 +0900

    refactor: 라우팅 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -30,36 +30,26 @@ function App() {
         <MainScreen>
           <ThemeProvider theme={theme}>
             <Routes>
-              <Route path="/" element={<EnterMain />}>
-                <Route path="login" element={<EnterLogin />}>
-                  <Route path="clause" element={<EnterClause />}>
-                    <Route path="profile" element={<EnterProfile />}>
-                      <Route path="guide" element={<EnterGuide />} />
-                    </Route>
-                  </Route>
-                </Route>
-              </Route>
-              <Route path="group" element={<ShareGroupMain />}>
-                <Route path="detail" element={<ShareGroupDetailPage />} />
-                <Route path="add" element={<AddGroupInputCount />}>
-                  <Route path="groupname" element={<AddGroupSpace />} />
-                  <Route path="membername" element={<AddGroupMemberName />} />
-                  <Route path="grouptype" element={<AddGroupType />} />
-                  <Route path="loading" element={<AddGroupLoading />} />
-                  <Route path="groupshare" element={<AddGroupShare />} />
-                </Route>
-                <Route path="join" element={<Joingroup1 />}>
-                  <Route path=":id" element={<Joingroup2 />}>
-                    <Route path="profile" element={<Joingroup3 />} />
-                  </Route>
-                </Route>
-              </Route>
-              <Route path="vote" element={<VoteMainPage />}>
-                <Route path="list" element={<VoteMainPage />} />
-                <Route path="create" element={<VoteMainPage />} />
-                <Route path="excute" element={<VoteMainPage />} />
-                <Route path="detail" element={<VoteMainPage />} />
-              </Route>
+              <Route path="/" element={<EnterMain />} />
+              <Route path="login" element={<EnterLogin />} />
+              <Route path="login/clause" element={<EnterClause />} />
+              <Route path="login/profile" element={<EnterProfile />} />
+              <Route path="login/profile/guide" element={<EnterGuide />} />
+              <Route path="group" element={<ShareGroupMain />} />
+              <Route path="group/detail" element={<ShareGroupDetailPage />} />
+              <Route path="group/add/member" element={<AddGroupMemberName />} />
+              <Route path="group/add/space" element={<AddGroupSpace />} />
+              <Route path="group/add/grouptype" element={<AddGroupType />} />
+              <Route path="group/add/loading" element={<AddGroupLoading />} />
+              <Route path="group/add/groupshare" element={<AddGroupShare />} />
+              <Route path="group/join" element={<Joingroup1 />} />
+              <Route path="group/join/:id" element={<Joingroup2 />} />
+              <Route path="group/join/:id/profile" element={<Joingroup3 />} />
+              <Route path="vote" element={<VoteMainPage />} />
+              <Route path="vote/list" element={<VoteMainPage />} />
+              <Route path="vote/create" element={<VoteMainPage />} />
+              <Route path="vote/excute" element={<VoteMainPage />} />
+              <Route path="vote/detail" element={<VoteMainPage />} />
             </Routes>
           </ThemeProvider>
         </MainScreen>
