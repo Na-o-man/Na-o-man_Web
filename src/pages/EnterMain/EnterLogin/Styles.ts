9486bbc6f8a9f0775941e6061d5657bc62b11f1b
khg5357 <rlagprud5356@gmail.com>
2024-08-02 15:15:09 +0900

    feat : enter페이지 로그인

--- src/pages/EnterMain/EnterLogin/Styles.ts
+++ src/pages/EnterMain/EnterLogin/Styles.ts
@@ -1,5 +1,14 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import * as I from 'assets/icon';
+
+export const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 export const LayOut = styled.div`
   z-index: 20;
@@ -19,4 +28,27 @@ export const KakaoLogin = styled(I.KakaoLogin)`
   width: 100%;
   top: 70%;
   cursor: pointer;
+`;
+export const Symbol = styled.img`
+  width: 20%;
+  position: absolute;
+  top: 25%;
+  left: 5%;
+  z-index: 20;
+  animation: ${rotate} 5s linear infinite;
+`;
+
+export const Background = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 10;
+`;
+
+export const Logo = styled.img`
+  z-index: 10;
+  width: 70%;
+  position: absolute;
+  top: 25%;
+  left: 15%;
 `;
