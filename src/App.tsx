3f168363f04978dd77bb4ae702a2de77f5a4beb7
leehyeon <liojung518@gmail.com>
2024-08-03 01:33:17 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+<<<<<<< Updated upstream
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/colors';
 import AddGroupInputCount from './pages/AddGroupMain/AddGroupInputCount';
@@ -31,6 +32,7 @@ function App() {
         <MainScreen>
           <ThemeProvider theme={theme}>
             <Routes>
+<<<<<<< Updated upstream
               <Route path="/" element={<EnterMain />} />
               <Route path="login" element={<EnterLogin />} />
               <Route path="login/clause" element={<EnterClause />} />
@@ -52,6 +54,14 @@ function App() {
               <Route path="vote/create" element={<VoteMainPage />} />
               <Route path="vote/excute" element={<VoteMainPage />} />
               <Route path="vote/detail" element={<VoteMainPage />} />
+=======
+              <Route path="/head-count" element={<AddGroupInputCount />} />
+              <Route path="/Space" element={<AddGroupSpace />} />
+              <Route path="/member-name" element={<AddGroupMemberName />} />
+              <Route path="/group-type" element={<AddGroupType />} />
+              <Route path="/loading" element={<AddGroupLoading />} />
+              <Route path="/share" element={<AddGroupShare />} />
+>>>>>>> Stashed changes
             </Routes>
           </ThemeProvider>
         </MainScreen>
