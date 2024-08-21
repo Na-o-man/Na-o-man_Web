ac6b17290ba04108a14befc813d34f9934ee1401
eomseona <eomsa98@tukorea.ac.kr>
2024-08-21 14:42:56 +0900

    fix : 안건 페이지 오류 해결

--- src/components/ShareGroup/ShareGroupListView/ShareGruopListView.tsx
+++ src/components/ShareGroup/ShareGroupListView/ShareGruopListView.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ShareGroupListItem from '../ShareGroupListItem/ShareGroupListItem';
 import * as S from './Styles';
 import { StyledNavLink } from 'components/Common/DropDown/Styles';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import {
   selectedGroupName,
   shareGroupListState,
 } from 'recoil/states/share_group';
 
 const ShareGruopListView: React.FC = () => {
-  const [shareGroupList] = useRecoilState(shareGroupListState);
+  const shareGroupList = useRecoilValue(shareGroupListState);
   const setSelectedGroup = useSetRecoilState(selectedGroupName);
 
   return (
