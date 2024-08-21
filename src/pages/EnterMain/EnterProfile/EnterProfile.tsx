04afd14857f0c34efc54550cf47c81fecf4a8e1b
eomseona <eomsa98@tukorea.ac.kr>
2024-08-21 21:46:57 +0900

    fix: 콘솔추가

--- src/pages/EnterMain/EnterProfile/EnterProfile.tsx
+++ src/pages/EnterMain/EnterProfile/EnterProfile.tsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 const EnterProfile = () => {
   const [user, setUserInfo] = useRecoilState(UserState);
   const token = useRecoilValue(accessToken);
-  console.log(token);
+  console.log(`token: ${token}`);
   useEffect(() => {
     const fetchUserData = async () => {
       if (!user && token) {
