f081b8ab03e91cd2d0c041d66f719aeeaa79c153
eomseona <eomsa98@tukorea.ac.kr>
2024-08-23 23:34:58 +0900

    fix: 타입 에러

--- src/pages/ShareGroup/ShareGroupDetailPage/ShareGroupDetailPage.tsx
+++ src/pages/ShareGroup/ShareGroupDetailPage/ShareGroupDetailPage.tsx
@@ -40,14 +40,14 @@ const ShareGroupDetailPage: React.FC = () => {
       size: 20,
     };
     try {
-      if (NumProfileId) {
+      if (NumProfileId === 0) {
         const { status, data } = await getPhotosAll(reqDataWithPage);
         if (status === 200) {
           console.log(data);
           setItems(data.photoInfoList);
           setMaxPage(data.totalPages);
         }
-      } else if (NumProfileId) {
+      } else if (NumProfileId === -1) {
         const { status, data } = await getPhotosEtc(reqDataWithPage);
         if (status === 200) {
           console.log(data);
