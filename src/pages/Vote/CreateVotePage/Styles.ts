1162ab3a5fc7a7fe65ec3fdd74d0956e51c6b393
khg5357 <rlagprud5356@gmail.com>
2024-08-23 16:57:46 +0900

    Docs : 디자인 수정 및 경로 수정

--- src/pages/Vote/CreateVotePage/Styles.ts
+++ src/pages/Vote/CreateVotePage/Styles.ts
@@ -2,9 +2,16 @@ import styled from 'styled-components';
 
 export const Layout = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
+  position: absolute;
+`;
+
+export const Container = styled.div`
+  display: grid;
+  position: absolute;
+  top: 15%;
+  justify-content: center;
 `;
